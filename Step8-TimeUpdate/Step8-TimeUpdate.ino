b62039047195272eccebb314b8df1679b7d8d89d
tretyakovsa <tretyakov.sa@gmail.com>
2019-08-08 23:35:25 +0300

    Добавлен шаг 8

---
+++ Step8-TimeUpdate/Step8-TimeUpdate.ino
@@ -0,0 +1,70 @@
+/*
+   Шаг №8
+   Обнавление прошивки из web интерфейса
+   Получение времени из сети
+
+*/
+#include <WiFi.h>        //Содержится в пакете. Видео с уроком http://esp8266-arduinoide.ru/step1-wifi
+#include <WebServer.h>   //Содержится в пакете. Видео с уроком http://esp8266-arduinoide.ru/step2-webserver
+#include <ESP32SSDP.h>        //https://github.com/luc-github/ESP32SSDP
+//                    ПЕРЕДАЧА ДАННЫХ НА WEB СТРАНИЦУ. Видео с уроком http://esp8266-arduinoide.ru/step5-datapages/
+//                    ПЕРЕДАЧА ДАННЫХ С WEB СТРАНИЦЫ.  Видео с уроком http://esp8266-arduinoide.ru/step6-datasend/
+#include <Update.h>
+#include <ArduinoJson.h>        //Установить из менеджера библиотек.
+
+#define FILESYSTEM SPIFFS
+// You only need to format the filesystem once
+#define FORMAT_FILESYSTEM false
+#define DBG_OUTPUT_PORT Serial
+
+#if FILESYSTEM == FFat
+#include <FFat.h>
+#endif
+#if FILESYSTEM == SPIFFS
+#include <SPIFFS.h>
+#endif
+
+IPAddress apIP(192, 168, 4, 1);
+
+// Web интерфейс для устройства
+WebServer HTTP(80);
+// Для файловой системы
+File fsUploadFile;
+
+// Определяем переменные wifi
+String _ssid     = "home"; // Для хранения SSID
+String _password = "i12345678"; // Для хранения пароля сети
+String _ssidAP = "WiFi";   // SSID AP точки доступа
+String _passwordAP = ""; // пароль точки доступа
+String SSDP_Name = "Update-Time"; // Имя SSDP
+int timezone = 3;               // часовой пояс GTM
+
+String jsonConfig = "{}";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("");
+  //Запускаем файловую систему
+  Serial.println("Start 4-FS");
+  FS_init();
+  Serial.println("Step7-FileConfig");
+  loadConfig();
+  Serial.println("Start 1-WIFI");
+  //Запускаем WIFI
+  WIFIinit();
+  Serial.println("Start 8-Time");
+  // Получаем время из сети
+  Time_init();
+  //Настраиваем и запускаем SSDP интерфейс
+  Serial.println("Start 3-SSDP");
+  SSDP_init();
+  //Настраиваем и запускаем HTTP интерфейс
+  Serial.println("Start 2-WebServer");
+  HTTP_init();
+
+}
+
+void loop() {
+  HTTP.handleClient();
+  delay(1);
+}
